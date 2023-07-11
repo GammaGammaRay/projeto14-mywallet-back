08858b4ed63dc239b54f9f4cc4d6db482f84a78b
GammaGammaRay <benjamin.peret@gmail.com>
2023-07-11 00:43:53 -0300

    deploy test

---
+++ src/controllers/transactionController.js
@@ -0,0 +1,15 @@
+import db from "../database/mongoDB";
+import { ObjectId } from "mongodb";
+import { stripHtml } from "string-strip-html";
+
+const listTransactions = async(req, res) => {
+    const userID = res.locals.userId;
+    try {
+        const user = await db.collection("users").findOne({_id: userID});
+        if (!user) return res.status(404).send("User not found");
+        const transactions = await db.collection("transactions").find({userID}).sort({$natural: -1}).toArray();
+        res.send(transactions);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
---
+++ src/database/mongoDB.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+  await mongoClient.connect();
+  console.log(chalk.bold.green("MongoDB OK"));
+} catch (err) {
+  console.log(chalk.bold.red(err.message));
+}
+
+const db = mongoClient.db();
+
+export default db;
